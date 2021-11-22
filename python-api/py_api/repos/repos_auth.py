f508140c5b530a1dcfbb7544e8a1983ce448ed29
Fabian Petersen <fabian.petersen@stud.hs-flensburg.de>
2021-11-22 23:38:06 +0100

    Endpunkt zum Logout /logout erstellt
    /logout löscht den "jib" cookie und sendet den String "logout" als Antwort

--- python-api/py_api/repos/repos_auth.py
+++ python-api/py_api/repos/repos_auth.py
@@ -33,6 +33,11 @@ async def login(response: Response, form_data: OAuth2PasswordRequestForm = Depen
     return schemas_user.UserToken(access_token=jwt_token.create_access_token(user_obj), token_type='bearer')
 
 
+async def logout(response: Response) -> str:
+    response.delete_cookie(key='jib', path='/refresh_token')
+    return "logout"
+
+
 async def refresh_token(request: Request, response: Response) -> schemas_user.UserToken:
     exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Refresh Token invalid')
     try:
--- python-api/py_api/routers/routers_auth.py
+++ python-api/py_api/routers/routers_auth.py
@@ -14,7 +14,12 @@ async def login(response: Response, form_data: OAuth2PasswordRequestForm = Depen
     return await repos_auth.login(response, form_data)
 
 
-@router.get("/refresh_token", response_model=schemas_user.UserToken)
+@router.post('/logout', response_model=schemas_user.UserToken)
+async def logout(response: Response) -> str:
+    return await repos_auth.logout(response)
+
+
+@router.post("/refresh_token", response_model=schemas_user.UserToken)
 async def refresh_token(request: Request, response: Response) -> schemas_user.UserToken:
     return await repos_auth.refresh_token(request, response)
 
