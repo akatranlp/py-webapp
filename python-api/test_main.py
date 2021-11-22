dfc355183c4eb8739dcc436a370980efcd0e2104
Fabian Petersen <fabian.petersen@stud.hs-flensburg.de>
2021-11-22 14:08:49 +0100

    Test zum Lesen aller Benutzer hinzugefügt
    In der Config wird die DB URL auf memory gesetzt, um nicht die Standard DB aus der Config
    zu benutzen, somit erhalten wir eine leere DB

--- python-api/test_main.py
+++ python-api/test_main.py
@@ -2,6 +2,11 @@ import asyncio
 from typing import Generator
 import pytest
 from fastapi.testclient import TestClient
+
+from py_api.config import set_config_value
+
+set_config_value('DATABASE_URL', 'sqlite://:memory:')
+
 from main import app
 
 @pytest.fixture(scope="module")
@@ -19,4 +24,10 @@ def test_read_main(client: TestClient):
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {'success': True, 'message': 'Hello World'}
+
+
+def test_read_users(client: TestClient):
+    response = client.get("/users")
+    assert response.status_code == 200
+    assert response.json() == []
 
