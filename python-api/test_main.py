59a371e4398c7fe15738f1d76f1943ae3ac943b0
Fabian Petersen <fabian.petersen@stud.hs-flensburg.de>
2021-11-22 14:06:44 +0100

    Veränderung des Grundaufbaus und Möglichkeit für AsyncTests

--- python-api/test_main.py
+++ python-api/test_main.py
@@ -1,10 +1,21 @@
+import asyncio
+from typing import Generator
+import pytest
 from fastapi.testclient import TestClient
 from main import app
 
-client = TestClient(app)
+@pytest.fixture(scope="module")
+def client() -> Generator:
+    with TestClient(app) as c:
+        yield c
 
 
-def test_read_main():
+@pytest.fixture(scope="module")
+def event_loop(client: TestClient) -> Generator:
+    yield asyncio.get_event_loop()
+
+
+def test_read_main(client: TestClient):
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {'success': True, 'message': 'Hello World'}
