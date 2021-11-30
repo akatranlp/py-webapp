f6cdc5e7bdfcaefdecc72a35945734591248a216
Fabian Petersen <fabian.petersen@stud.hs-flensburg.de>
2021-11-30 12:53:57 +0100

    Plugins laden mithilfe des PluginLoaders
    es werden die Plugins geladen, die Routen hinzugefügt, die Models hinzugefügt,
    die neededKeys registriert und die static-Files gemountet,
    wenn die Plugins weitere Überprüfungsprozesse überstanden haben.

--- python-api/main.py
+++ python-api/main.py
@@ -1,3 +1,5 @@
+import warnings
+from py_api.plugins.plugin_helper import PluginLoader
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from py_api.routers import routers_user, routers_auth, routers_client
@@ -5,15 +7,54 @@ from tortoise.contrib.fastapi import register_tortoise
 from py_api.config import Config
 
 Config.get_instance().register_needed_keys(keys=['JWT_REFRESH_TOKEN_SECRET', 'JWT_ACCESS_TOKEN_SECRET'])
+
+models = ['py_api.models.models_user']
+
+plugin_loader = PluginLoader()
+
+plugin_loader.load_plugins()
+print_text = 'Plugin {} could not be loaded because {}'
+
+routers_to_add = []
+
+for plugin in plugin_loader.plugins:
+    routers = plugin.serve_routers()
+
+    correct_routers = True
+
+    for router in routers:
+        if not router.prefix.startswith(f'/plugin/{plugin.name}'):
+            correct_routers = False
+            break
+
+    if not correct_routers:
+        warnings.warn(print_text.format(plugin.name, 'the router is not correctly defined'))
+        plugin_loader.plugins.remove(plugin)
+        continue
+
+    routers_to_add.append(*routers)
+
+    models.append(*plugin.models)
+
+    if plugin.needed_env_keys is not None and plugin.needed_env_keys is not []:
+        Config.get_instance().register_needed_keys(keys=plugin.needed_env_keys)
+
 Config.get_instance().validate_needed_keys()
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
-
 app.include_router(routers_auth.router)
 app.include_router(routers_user.router)
 app.include_router(routers_client.router)
+
+for router in routers_to_add:
+    app.include_router(router)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+for plugin in plugin_loader.plugins:
+    if plugin.has_static_files:
+        app.mount(f'/{plugin.name}', StaticFiles(directory=f'py_api/plugins/{plugin.name}/static'), name=plugin.name)
 
 
 @app.get("/hello-world")
@@ -27,7 +68,7 @@ def hello():
 register_tortoise(
     app,
     db_url=Config.get_instance().get_config_value('DATABASE_URL', 'sqlite://db.sqlite3'),
-    modules={'models': ['py_api.models.models_user']},
+    modules={'models': models},
     generate_schemas=True,
     add_exception_handlers=True,
 )
