6bd6a90200979584a9a89d8f31a8fdd11581000d
Pascal Friedrichsen <pascal.friedrichsen@stud.hs-flensburg.de>
2021-12-12 13:07:57 +0100

    ToDo-Model erstellt

---
+++ python-api/py_api/models/models_todo.py
@@ -0,0 +1,10 @@
+from tortoise import fields
+from tortoise.models import Model
+from .models_helper import DBBaseModel
+
+
+class Todo(DBBaseModel, Model):
+    title = fields.CharField(128)
+    status = fields.BooleanField ## Offen(0) oder Erledigt(1)
+    description = fields.TextField(null=True)
+    creator = fields.ForeignKeyField("models.User", related_name="todo")
