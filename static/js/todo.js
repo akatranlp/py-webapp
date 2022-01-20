7e0528e3c3cfce20ab6812454f21333f177573db
Pascal Friedrichsen <pascal.friedrichsen@stud.hs-flensburg.de>
2022-01-20 15:21:46 +0100

    todos now get loaded into their respecting container (active/finished)

--- static/js/todo.js
+++ static/js/todo.js
@@ -4,36 +4,56 @@ const deleteButton = document.querySelector("[data-delete-button]");
 const changeStateButton = document.querySelector("[data-change-state-button]");
 const activeContainer = document.querySelector("[data-active-container]");
 const finishedContainer = document.querySelector("[data-finished-container]");
-async function loadData(){
+
+async function loadData() {
     //Wird beim Laden aufgerufen und gibt dem HTML alles, was wir brauchen
     const resp = await axiosInstance.get("/todos")
     const todos = resp.data
-    loadTodo("a")
-
-}
-function changeStatus(uuid){
-console.log("HAHAHAHAHAHAH")
+    console.log(todos)
+    todos.forEach(el => loadTodo(el))
 }
 
-function deleteTodo(uuid){
+function changeStatus(uuid) {
+    console.log("HAHAHAHAHAHAH")
+}
+
+function deleteTodo(uuid) {
 
 }
-//Lädt die Daten
-function loadTodo(curTodo){
+
+//Lädt die Todos
+function loadTodo(curTodo) {
     let todoObject
-        todoObject = finishedContainer.createElement('div')
-        todoObject.className = "p-2 border border-info rounded highlight"
-        let title = todoObject.createElement('h2')
-        title.innerText = curTodo.title;
-        let description = todoObject.createElement('p')
-        description.innerText = curTodo.description;
-        let buttonDiv = todoObject.createElement('div')
-        let buttonChangeStatus = buttonDiv.createElement('a')
-        buttonChangeStatus.className="btn btn-success ml-2"
-        buttonChangeStatus.onclick = changeStatus(curTodo.uuid);
-        let buttonDelete = buttonDiv.createElement('a')
-        buttonDelete.className="btn btn-danger ml-2"
-        buttonDelete.onclick = deleteTodo(curTodo.uuid);
+    todoObject = document.createElement('div')
+    curTodo.status ? finishedContainer.appendChild(todoObject) : activeContainer.appendChild(todoObject)
+    todoObject.className = "p-2 border border-info rounded highlight mb-1"
+    let title = document.createElement('h2')
+    title.innerText = curTodo.title;
+    todoObject.appendChild(title)
+    let description = document.createElement('p')
+    description.innerText = curTodo.description;
+    todoObject.appendChild(description)
+    let buttonDiv = document.createElement('div')
+    buttonDiv.className = "d-flex justify-content-end"
+    todoObject.appendChild(buttonDiv)
+    let buttonChangeStatus = document.createElement('a')
+
+    buttonChangeStatus.addEventListener("click", () => changeStatus(curTodo.uuid))
+    if(curTodo.status) {
+        buttonChangeStatus.className = "btn btn-warning ml-2"
+        buttonChangeStatus.innerText = "Reaktivieren"
+    }
+    else{
+        buttonChangeStatus.className = "btn btn-success ml-2"
+        buttonChangeStatus.innerText = "Erledigt"
+    }
+    buttonDiv.appendChild(buttonChangeStatus)
+    let buttonDelete = document.createElement('a')
+    buttonDelete.className = "btn btn-danger ml-2"
+    buttonDelete.addEventListener("click", () => deleteTodo(curTodo.uuid))
+    buttonDelete.innerText = "Löschen"
+    buttonDiv.appendChild(buttonDelete)
 
 }
+
 loadData()
