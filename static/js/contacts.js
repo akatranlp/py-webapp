4a2430a17bcfb953ac6a7e0a386c36770c56201c
Dominik Heckner <dominik.heckner@stud.hs-flensburg.de>
2022-01-22 12:42:56 +0100

    Added some changes

--- static/js/contacts.js
+++ static/js/contacts.js
@@ -1,23 +1,34 @@
-import {axiosInstance} from "./repo";
+import {user, axiosInstance} from "./repo.js";
 
 const tableElement = document.querySelector("[data-table]")
 const formElement = document.querySelector("[data-form]")
+const logoutButton = document.getElementById("logout")
 const nameElement = document.getElementById("name")
 const firstnameElement = document.getElementById("firstname")
 const emailElement = document.getElementById("email")
+
+logoutButton.addEventListener("click", async (e) => {
+    console.log("SSSSSSSSSSS")
+    try {
+        await axiosInstance.get("/logout")
+    } catch (error) {
+
+    }
+})
 
 formElement.addEventListener("submit", async (e) => {
     e.preventDefault()
     const name = nameElement.value
     const firstname = firstnameElement.value
     const email = emailElement.value
-        try {
-            const response = await axiosInstance.post("/contacts", {name, firstname, email})
-            window.location = "/contact"
-        } catch (error) {
-            const data = error.response.data
-            alert(data.detail + " " + error.response.status)
-        }
+    try {
+        const response = await axiosInstance.post("/contacts", {name, firstname, email})
+        console.log(response)
+        window.location = "/contact"
+    } catch (error) {
+        const data = error.response.data
+        alert(data.detail + " " + error.response.status)
+    }
 })
 
 const loadData = async () => {
@@ -26,10 +37,10 @@ const loadData = async () => {
 
         contacts.forEach(element => {
             const contact = document.createElement("td")
-            contact.innerHTML = element
+            contact.innerHTML = element.name
             tableElement.appendChild(contact)
         })
-    }catch (error) {
+    } catch (error) {
 
     }
 }
