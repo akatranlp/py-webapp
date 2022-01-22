91f21658985f22184f59140e03170a2fc94d4c0b
Fabian Petersen <fabian.petersen@stud.hs-flensburg.de>
2022-01-22 18:13:22 +0100

    start of the index site
    it is now implemented the paths if we are logged in or not

--- static/js/index.js
+++ static/js/index.js
@@ -1,2 +1,23 @@
+const meElement = document.querySelector("[data-me]")
 
-console.log("Hello World");
+const getAccessToken = async () => {
+    const resp = await axios.get('/refresh_token')
+    return resp.data.token_type === 'bearer' ? resp.data.access_token : null
+}
+
+const getMe = async () => {
+    const access_token = await getAccessToken()
+    const resp = await axios.get('/users/me', {headers: {Authorization: `Bearer ${access_token}`}})
+    return resp.data
+}
+
+const renderIndex = async () => {
+    try {
+        const me = await getMe()
+        console.log(me)
+        meElement.innerText = me.username
+    } catch (e) {
+    }
+}
+
+renderIndex()
