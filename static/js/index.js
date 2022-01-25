a4d1d37c0458b3a1d0c60a3068cb21fccd32582d
Kajsa Heuer <kajsa.heuer@stud.hs-flensburg.de>
2022-01-25 12:09:23 +0100

    implemented render Plugins and changed the system how the login status is rendered

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,8 @@
-const meElement = document.querySelector("[data-me]")
+const navLoggedInElement = document.querySelector("[data-nav-LoggedIn]");
+const navLoggedOutElement = document.querySelector("[data-nav-LoggedOut]");
+const pluginsContainerElement = document.querySelector("[data-plugins-container]");
+
+let me;
 
 const getAccessToken = async () => {
     const resp = await axios.get('/refresh_token')
@@ -11,13 +15,55 @@ const getMe = async () => {
     return resp.data
 }
 
-const renderIndex = async () => {
+const renderIsLoggedIn = async () => {
     try {
-        const me = await getMe()
-        console.log(me)
-        meElement.innerText = me.username
+        me = await getMe()
+        renderLoggedIn()
     } catch (e) {
+        me = null
+        renderLoggedOut()
     }
+}
+
+const renderLoggedIn = () => {
+    navLoggedOutElement.remove()
+    navLoggedInElement.hidden = false
+    const meElement = document.querySelector("[data-me]");
+    meElement.innerText = me.username
+}
+
+const renderLoggedOut = () => {
+    navLoggedInElement.remove()
+    navLoggedOutElement.hidden = false
+}
+
+const renderPlugins = async () => {
+    const resp = await axios.get('/plugins')
+    const plugins = resp.data
+    if (plugins.length > 1) {
+        const pluginsHeader = document.createElement('h1');
+        pluginsHeader.innerText = 'Folgende Plugins sind installiert'
+        pluginsContainerElement.appendChild(pluginsHeader);
+    }
+
+    plugins.forEach(plugin => {
+        let routeElements = ''
+        for(const route of plugin.routes) {
+            routeElements += `<li><a href="${route}">${route}</a></li>`
+        }
+
+        const pluginContainer = document.createElement('div');
+        pluginContainer.innerHTML = `
+        <h2>${plugin.name}</h2>
+        ${routeElements ? "<ul>"+routeElements+"</ul>" : ""}
+        `
+        pluginsContainerElement.appendChild(pluginContainer);
+    })
+}
+
+const renderIndex = async () => {
+    await renderIsLoggedIn()
+    await renderPlugins()
 }
 
 renderIndex()
