6b29fcd45d595267bf21657444586768209376e4
Fabian Petersen <fabian.petersen@stud.hs-flensburg.de>
2022-01-26 20:54:59 +0100

    added the functionality to add participants when creating events and change them with put events

--- py_api/repos/repos_event.py
+++ py_api/repos/repos_event.py
@@ -3,17 +3,45 @@ from uuid import UUID
 
 from fastapi import HTTPException, status
 from ..schemas import schemas_event
-from ..models import models_event, models_user
+from ..models import models_event, models_user, models_contact
 
 
-async def get_all(user: models_user.User) -> List[schemas_event.EventOut]:
+async def get_all(user: models_user.User) -> List[schemas_event.Event]:
     event_list = []
     async for event_obj in models_event.Event.filter(creator=user):
-        event_list.append(await schemas_event.EventOut.from_tortoise_orm(event_obj))
+        event_list.append(await schemas_event.Event.from_tortoise_orm(event_obj))
     return event_list
 
 
-# TODO participants fehlen noch
+async def event_participant_in_to_model(event_participant: schemas_event.EventParticipantIn, event_uuid):
+    try:
+        event_obj = await models_event.Event.get(uuid=event_uuid)
+    except:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Event not found')
+    try:
+        contact_obj = await models_contact.Contact.get(uuid=event_participant.contact_uuid)
+    except:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Contact not found')
+    try:
+        user_obj = await models_user.User.get(email=contact_obj.email)
+    except:
+        user_obj = None
+    if user_obj:
+        status_obj = await models_event.EventParticipantStatus.get(status="Pending")
+    else:
+        status_obj = await models_event.EventParticipantStatus.get(status="Accepted")
+    event_participant_obj = models_event.EventParticipant(
+        event=event_obj,
+        contact=contact_obj,
+        user=user_obj,
+        status=status_obj
+    )
+    try:
+        await event_participant_obj.save()
+    except:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Hat nicht funktioniert')
+
+
 async def create_event(event: schemas_event.EventIn, user: models_user.User) -> schemas_event.EventOut:
     event_obj = models_event.Event(
         title=event.title,
@@ -21,13 +49,16 @@ async def create_event(event: schemas_event.EventIn, user: models_user.User) -> 
         end_date=event.end_date,
         description=event.description,
         creator=user,
-        # participants = None,
         location=event.location)
     try:
         await event_obj.save()
     except:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Hat nicht funktioniert')
-    return await schemas_event.EventOut.from_tortoise_orm(event_obj)
+
+    if event.participants:
+        for p in set([p.contact_uuid for p in event.participants]):
+            await event_participant_in_to_model(schemas_event.EventParticipantIn(contact_uuid=p), event_obj.uuid)
+    return await schemas_event.EventOut.from_model(event_obj)
 
 
 async def _get_event(uuid: UUID, user: models_user.User):
@@ -38,12 +69,24 @@ async def _get_event(uuid: UUID, user: models_user.User):
 
 
 async def get_event(uuid: UUID, user: models_user.User):
-    return await schemas_event.EventOut.from_tortoise_orm(await _get_event(uuid, user))
+    return await schemas_event.EventOut.from_model(await _get_event(uuid, user))
 
 
-# TODO participants fehlen noch
 async def change_event(uuid: UUID, event: schemas_event.EventPut, user: models_user.User):
     event_obj = await _get_event(uuid, user)
+    if event.participants:
+        participants = []
+        async for participant in models_event.EventParticipant.filter(event=event_obj):
+            participants.append(await schemas_event.EventParticipantOut.from_model(participant))
+        before_participant_set = set([p.contact_uuid for p in participants])
+        after_participant_set = set([p.contact_uuid for p in event.participants])
+        intersection = before_participant_set & after_participant_set
+        for p in before_participant_set:
+            if p not in intersection:
+                await (await models_event.EventParticipant.get(contact_id=p, event=event_obj)).delete()
+        for p in after_participant_set:
+            if p not in intersection:
+                await event_participant_in_to_model(schemas_event.EventParticipantIn(contact_uuid=p), event_obj.uuid)
     if event.title:
         event_obj.title = event.title
     if event.start_date:
@@ -52,16 +95,15 @@ async def change_event(uuid: UUID, event: schemas_event.EventPut, user: models_u
         event_obj.end_date = event.end_date
     if event.description:
         event_obj.description = event.description
-    # participants: Optional[List[schemas_user.UserOut]]  # muss ContactOut sein
     if event.location:
         event_obj.location = event.location
 
     await event_obj.save()
-    return await schemas_event.EventOut.from_tortoise_orm(event_obj)
+    return await schemas_event.EventOut.from_model(event_obj)
 
 
 async def delete_event(uuid: UUID, user: models_user.User):
     event_obj = await _get_event(uuid, user)
-    event = await schemas_event.EventOut.from_tortoise_orm(event_obj)
+    event = await schemas_event.EventOut.from_model(event_obj)
     await event_obj.delete()
     return event
--- py_api/schemas/schemas_event.py
+++ py_api/schemas/schemas_event.py
@@ -1,18 +1,97 @@
 import datetime
+from uuid import UUID
 from tortoise.contrib.pydantic import pydantic_model_creator
 from pydantic import BaseModel, Extra
 from typing import Optional, List
 from ..models import models_event
-from ..schemas import schemas_user
+
+EventParticipant = pydantic_model_creator(models_event.EventParticipant, name="EventParticipant")
+
+
+class EventParticipantIn(BaseModel):
+    contact_uuid: UUID
+
+    class Config:
+        extra = Extra.forbid
+
+
+class EventParticipantOut(BaseModel):
+    contact_uuid: UUID
+    contact_firstname: str
+    contact_name: str
+    contact_email: str
+    user_id: Optional[int]
+    status: str
+
+    class Config:
+        extra = Extra.forbid
+
+    @classmethod
+    async def from_model(cls, model: models_event.EventParticipant):
+        user = await model.user
+        contact = await model.contact
+        return EventParticipantOut(
+            contact_uuid=contact.uuid,
+            contact_firstname=contact.firstname,
+            contact_name=contact.name,
+            contact_email=contact.email,
+            user_id=user.id if user else None,
+            status=(await model.status).status
+        )
+
 
 Event = pydantic_model_creator(models_event.Event, name="Event")
 
-EventIn = pydantic_model_creator(models_event.Event, name="EventIn",
-                                 exclude_readonly=True,
-                                 exclude=('participants', 'system_id', 'created_at', 'updated_at', 'creator'))
 
-EventOut = pydantic_model_creator(models_event.Event, name="EventOut",
-                                  exclude=('participants',))
+class EventIn(BaseModel):
+    title: str
+    start_date: datetime.datetime
+    end_date: datetime.datetime
+    description: Optional[str]
+    participants: Optional[List[EventParticipantIn]]
+    location: Optional[str]
+
+    class Config:
+        extra = Extra.forbid
+
+
+class EventOut(BaseModel):
+    uuid: UUID
+    system_id: str
+    created_at: datetime.datetime
+    updated_at: datetime.datetime
+    title: str
+    start_date: datetime.datetime
+    end_date: datetime.datetime
+    description: Optional[str]
+    participants: Optional[List[EventParticipantOut]]
+    location: Optional[str]
+    creator_username: str
+    creator_email: str
+
+    class Config:
+        extra = Extra.forbid
+
+    @classmethod
+    async def from_model(cls, model: models_event.Event):
+        participants = []
+        async for participant in models_event.EventParticipant.filter(event=model):
+            participants.append(await EventParticipantOut.from_model(participant))
+        creator = await model.creator
+        return EventOut(
+            uuid=model.uuid,
+            system_id=model.system_id,
+            created_at=model.created_at,
+            updated_at=model.updated_at,
+            title=model.title,
+            start_date=model.start_date,
+            end_date=model.end_date,
+            description=model.description,
+            participants=participants,
+            location=model.location,
+            creator_username=creator.username,
+            creator_email=creator.email
+        )
 
 
 class EventPut(BaseModel):
@@ -20,7 +99,7 @@ class EventPut(BaseModel):
     start_date: Optional[datetime.datetime]
     end_date: Optional[datetime.datetime]
     description: Optional[str]
-    participants: Optional[List[schemas_user.UserOut]]  # muss ContactOut sein
+    participants: Optional[List[EventParticipantIn]]
     location: Optional[str]
 
     class Config:
