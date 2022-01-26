e56183bf40bb0b486b0e727c64833fc5c6c88978
Fabian Petersen <fabian.petersen@stud.hs-flensburg.de>
2022-01-26 21:42:32 +0100

    implemented the endpoints for the entries

--- py_api/routers/routers_event.py
+++ py_api/routers/routers_event.py
@@ -46,3 +46,34 @@ async def delete_event(uuid: UUID,
                        user: models_user.User =
                        Depends(oauth2.get_current_active_user_model)) -> schemas_event.EventOut:
     return await repos_event.delete_event(uuid, user)
+
+
+@router.get('/{uuid}/entries', response_model=List[schemas_event.EventParticipantOut])
+async def get_all_entries(uuid: UUID,
+                          user: models_user.User =
+                          Depends(oauth2.get_current_active_user_model)) -> List[schemas_event.EventParticipantOut]:
+    return await repos_event.get_all_entries(uuid, user)
+
+
+@router.post('/{uuid}/entries', response_model=schemas_event.EventParticipantOut)
+async def create_entry(uuid: UUID,
+                       participant: schemas_event.EventParticipantIn,
+                       user: models_user.User =
+                       Depends(oauth2.get_current_active_user_model)) -> schemas_event.EventParticipantOut:
+    return await repos_event.create_entry(uuid, participant, user)
+
+
+@router.get('/{uuid}/entries/{contact_uuid}', response_model=schemas_event.EventParticipantOut)
+async def get_entry(uuid: UUID,
+                    contact_uuid: UUID,
+                    user: models_user.User =
+                    Depends(oauth2.get_current_active_user_model)) -> schemas_event.EventParticipantOut:
+    return await repos_event.get_entry(uuid, contact_uuid, user)
+
+
+@router.delete('/{uuid}/entries/{contact_uuid}', response_model=schemas_event.EventParticipantOut)
+async def delete_entry(uuid: UUID,
+                       contact_uuid: UUID,
+                       user: models_user.User =
+                       Depends(oauth2.get_current_active_user_model)) -> schemas_event.EventParticipantOut:
+    return await repos_event.delete_entry(uuid, contact_uuid, user)
