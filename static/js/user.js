62ee5d74f52d21dacc7760230899a707371d75d5
pascal.friedrichsen <pafr2628@hs-flensburg.de>
2022-02-03 01:03:11 +0100

    added safetymeasure to account deletion (now need to click a second button)

--- static/js/user.js
+++ static/js/user.js
@@ -5,63 +5,29 @@ const usersContainerElement = document.querySelector("[data-user-container]");
 const getAllUsers = async () => {
     usersContainerElement.innerHTML = ''
     const resp = await axiosInstance.get('/users')
-    // Table erstellen mit Header
-    const table = document.createElement('table')
-    const tableHeader = document.createElement('thead')
-    table.appendChild(tableHeader)
-    const tableHeaderRow = document.createElement('tr')
-    tableHeader.appendChild(tableHeaderRow)
-    {
-        const tableHead = document.createElement('th')
-        tableHead.innerText = "Name"
-        tableHeaderRow.appendChild(tableHead)
-    }
-    {
-        const tableHead = document.createElement('th')
-        tableHead.innerText = "E-Mail"
-        tableHeaderRow.appendChild(tableHead)
-    }
-    {
-        const tableHead = document.createElement('th')
-        tableHead.innerText = "Einstellungen"
-        tableHeaderRow.appendChild(tableHead)
-    }
-    const tableBody = document.createElement('tbody')
-    table.appendChild(tableBody)
     resp.data.forEach(user => {
         const userRow = document.createElement('tr');
         //userRow.className = "d-flex flex-row justify-content-between p-1 border border-info rounded highlight mb-1"
         {
             const nameElement = document.createElement('td');
             nameElement.innerText = user.username
+            nameElement.className = "align-middle"
             userRow.appendChild(nameElement)
         }
         {
             const emailElement = document.createElement('td');
             emailElement.innerText = user.email
+            emailElement.className = "align-middle"
             userRow.appendChild(emailElement)
         }
+
         const buttonElement = document.createElement('td');
+        buttonElement.className = "d-flex justify-content-end"
         userRow.appendChild(buttonElement)
-        const isActiveBtn = document.createElement('button');
-        isActiveBtn.className = `btn text-white mr-sm-2 ${user.is_active ? "btn-danger" : "btn-success"}`
-        isActiveBtn.innerText = user.is_active ? 'Deactivate' : 'Reactivate'
-        buttonElement.appendChild(isActiveBtn);
-
-        isActiveBtn.addEventListener('click', async () => {
-            const is_active = !user.is_active
-
-            try {
-                await axiosInstance.put('/users/' + user.username, {is_active})
-                await getAllUsers()
-            } catch (e) {
-                alert(e)
-            }
-        })
 
         const isAdminBtn = document.createElement('button');
         isAdminBtn.className = `btn text-white mr-sm-2 ${user.is_admin ? "btn-danger" : "btn-success"}`
-        isAdminBtn.innerText = user.is_admin ? 'Demote' : 'Promote'
+        isAdminBtn.innerText = user.is_admin ? 'Adminrechte entfernen' : 'Adminrechte geben'
         buttonElement.appendChild(isAdminBtn);
 
         isAdminBtn.addEventListener('click', async () => {
@@ -75,24 +41,47 @@ const getAllUsers = async () => {
             }
         })
 
-        const deleteBtn = document.createElement('button');
-        deleteBtn.className = 'btn text-white mr-sm-2 btn-danger'
-        deleteBtn.innerText = 'Delete'
-        buttonElement.appendChild(deleteBtn);
+        const isActiveBtn = document.createElement('button');
+        isActiveBtn.className = `btn text-white mr-sm-2 ${user.is_active ? "btn-danger" : "btn-success"}`
+        isActiveBtn.innerText = user.is_active ? 'Deaktivieren' : 'Reaktivieren'
+        buttonElement.appendChild(isActiveBtn);
 
-        deleteBtn.addEventListener('click', async () => {
-            // TODO: Popup ob man wirklich den User löschen möchte
+        isActiveBtn.addEventListener('click', async () => {
+            const is_active = !user.is_active
+
             try {
-                await axiosInstance.delete('/users/' + user.username)
+                await axiosInstance.put('/users/' + user.username, {is_active})
                 await getAllUsers()
             } catch (e) {
                 alert(e)
             }
         })
 
-        tableBody.appendChild(userRow);
+        const deleteBtnPre = document.createElement('button');
+        deleteBtnPre.className = 'btn text-white mr-sm-2 btn-danger'
+        deleteBtnPre.innerText = 'Löschen'
+        buttonElement.appendChild(deleteBtnPre);
+        deleteBtnPre.addEventListener('click', async () => {
+            //Beim klicken wird der Nachfrageknopf erstellt und deleteBtnPre gelöscht
+            const deleteBtn = document.createElement('button');
+            deleteBtn.className = 'btn text-white mr-sm-2 btn-dark'
+            deleteBtn.innerText = 'Sicher?'
+            buttonElement.appendChild(deleteBtn);
+
+            deleteBtn.addEventListener('click', async () => {
+                try {
+                    await axiosInstance.delete('/users/' + user.username)
+                    await getAllUsers()
+                } catch (e) {
+                    alert(e)
+                }
+            })
+            deleteBtnPre.remove()
+        })
+
+
+        usersContainerElement.appendChild(userRow);
     })
-    usersContainerElement.appendChild(table);
 }
 
 
