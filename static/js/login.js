c6e7fe70cbd0883ab6c6a46fb11949f31ed7efef
Fabian Petersen <fabian.petersen@stud.hs-flensburg.de>
2022-02-04 11:20:13 +0100

    refactored these files

--- static/js/login.js
+++ static/js/login.js
@@ -1,16 +1,17 @@
 const formElement = document.querySelector("[data-form]")
 const errorAlert = document.querySelector("[data-alert]");
 
-formElement.addEventListener("submit", async(e) => {
+formElement.addEventListener("submit", async (e) => {
     e.preventDefault()
 
+    // Wir benutzen hier fetch, da axios aktuell bugs mit form-data hat
     const data = new URLSearchParams(new FormData(formElement))
     try {
         const response = await fetch("/login", {method: "post", body: data})
         const jsonData = await response.json()
         if (response.status === 200) {
             window.location = "/"
-        } else if (response.status === 401){
+        } else if (response.status === 401) {
             openErrorAlert("Passwort ist falsch: " + response.status + " - " + jsonData.detail, null)
         } else {
             openErrorAlert("Username ist falsch: " + response.status + " - " + jsonData.detail, null)
@@ -24,7 +25,7 @@ const openErrorAlert = (text, e) => {
     errorAlert.className = "alert alert-danger p-1"
     if (e !== null) {
         errorAlert.innerText = text + ": " + e.response.status + " - " + e.response.statusText
-    }else {
+    } else {
         errorAlert.innerText = text
     }
     errorAlert.removeAttribute("hidden")
@@ -34,7 +35,7 @@ const checkLogin = async () => {
     try {
         await axios.get("/refresh_token")
         window.location = "/"
-    }catch (error){
+    } catch (error) {
 
     }
 }
